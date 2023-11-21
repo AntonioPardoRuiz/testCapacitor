76ee509f3316692ec8f22dc16aab5c30cf9e61a4
Antonio M Pardo Ruiz <antoniomiguelpardoruiz@gmail.com>
2023-11-21 10:24:21 +0100

    Añadimos los datos de device , battery al capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//Importamos los datos del dispositivo a partir de Capacitor
+import  {Device } from '@capacitor/device';
+//Importamos la informacion de Capacitor
+import {Capacitor} from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  //Para el plugin de capacitor
+  ptfName:any;
+  isNative:any;
+  isAvailable:any;
+  //PAra el plugin de Device
+  //Los definimos como any.
+  deviceInfo:any;
+  deviceId:any;
+  deviceBattery:any;
 
-  constructor() {}
+
+  constructor(){
+
+    this.ptfName= Capacitor.getPlatform();
+    this.isNative = Capacitor.isNativePlatform();
+    this.isAvailable = Capacitor.isPluginAvailable('Camera');
+
+    Device.getInfo().then(
+      result=> { this.deviceInfo = result;}),
+  
+    Device.getId().then(
+      result=> { this.deviceId = result;}),
+
+    Device.getBatteryInfo().then(
+         result=> { this.deviceBattery = result;})
+    }
 
 }
